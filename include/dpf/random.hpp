1e5d5636766aa58aab9f1874a0f7b961a5c0e9f4
Ryan Henry <ryan.henry@ucalgary.ca>
2023-01-19 08:56:50 -0700

    fixed copyright date

---
+++ include/dpf/random.hpp
@@ -0,0 +1,30 @@
+/// @file dpf/random.hpp
+/// @author Ryan Henry <ryan.henry@ucalgary.ca>
+/// @brief
+/// @copyright Copyright (c) 2019-2023 Ryan Henry and others
+/// @license Released under a GNU General Public v2.0 (GPLv2) license;
+///          see `LICENSE` for details.
+
+#ifndef LIBDPF_INCLUDE_DPF_RANDOM_HPP__
+#define LIBDPF_INCLUDE_DPF_RANDOM_HPP__
+
+#include <bsd/stdlib.h>
+
+#include "hedley/hedley.h"
+
+namespace dpf
+{
+
+template <typename T>
+HEDLEY_PURE
+HEDLEY_ALWAYS_INLINE
+HEDLEY_NO_THROW
+auto & uniform_fill(T & buf) noexcept
+{
+    arc4random_buf(&buf, sizeof(buf));
+    return buf;
+}
+
+}  // namespace dpf
+
+#endif  // LIBDPF_INCLUDE_DPF_RANDOM_HPP__
