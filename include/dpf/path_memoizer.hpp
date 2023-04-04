3009d1812a6349397bfae5a3cca451d8625bd797
Chris Jiang <chrisljiang@gmail.com>
2023-04-04 00:46:09 -0600

    Add `path_memoizer_base` class to better enforce interface

--- include/dpf/path_memoizer.hpp
+++ include/dpf/path_memoizer.hpp
@@ -13,15 +13,34 @@
 namespace dpf
 {
 
-template <typename DpfKey>
-struct alignas(alignof(typename DpfKey::interior_node_t))
-basic_path_memoizer final
-    : public std::array<typename DpfKey::interior_node_t, DpfKey::depth+1>
+template <typename DpfKey,
+          typename ReturnT = const typename DpfKey::interior_node_t *>
+struct path_memoizer_base
 {
   public:
     using dpf_type = DpfKey;
     using input_type = typename DpfKey::input_type;
-    using return_type = std::add_pointer_t<std::add_const_t<typename DpfKey::interior_node_t>>;
+    using node_type = typename DpfKey::interior_node_t;
+    using return_type = ReturnT;
+    using iterator_type = return_type;
+
+    virtual std::size_t assign_x(const dpf_type &, input_type) noexcept = 0;
+    virtual node_type & operator[](std::size_t) noexcept = 0;
+
+    virtual return_type begin() const noexcept = 0;
+    virtual return_type end() const noexcept = 0;
+};
+
+template <typename DpfKey>
+struct alignas(alignof(typename DpfKey::interior_node_t))
+basic_path_memoizer final
+    : public path_memoizer_base<DpfKey>
+{
+  public:
+    using dpf_type = DpfKey;
+    using input_type = typename DpfKey::input_type;
+    using node_type = typename DpfKey::interior_node_t;
+    using return_type = const typename DpfKey::interior_node_t *;
     using iterator_type = return_type;
     static constexpr auto depth = DpfKey::depth;
 
@@ -33,7 +52,7 @@ basic_path_memoizer final
     basic_path_memoizer & operator=(const basic_path_memoizer &) = default;
     ~basic_path_memoizer() = default;
 
-    inline std::size_t assign_x(const dpf_type & dpf, input_type new_x)
+    std::size_t assign_x(const dpf_type & dpf, input_type new_x) noexcept override
     {
         static constexpr auto clz_xor = utils::countl_zero_symmmetric_difference<input_type>{};
         if (dpf_.has_value() == true && std::addressof(dpf_->get()) == std::addressof(dpf))
@@ -50,11 +69,16 @@ basic_path_memoizer final
         return 1;
     }
 
-    return_type begin() const noexcept
+    node_type & operator[](std::size_t i) noexcept override
+    {
+        return arr_[i];
+    }
+
+    return_type begin() const noexcept override
     {
         if (x_.has_value() == true)
         {
-            return &this->operator[](dpf_type::depth);
+            return std::addressof(arr_[depth]);
         }
         else
         {
@@ -62,27 +86,25 @@ basic_path_memoizer final
         }
     }
 
-    return_type end() const noexcept
+    return_type end() const noexcept override
     {
-        return &this->operator[](dpf_type::depth+1);
+        return std::addressof(arr_[depth+1]);
     }
 
   private:
     std::optional<std::reference_wrapper<const dpf_type>> dpf_;
     std::optional<input_type> x_;
+    std::array<node_type, depth+1> arr_;
 };
 
 template <typename DpfKey>
-struct nonmemoizing_path_memoizer final
+struct nonmemoizing_path_memoizer final : public path_memoizer_base<DpfKey>
 {
   public:
     using dpf_type = DpfKey;
     using input_type = typename DpfKey::input_type;
     using node_type = typename DpfKey::interior_node_t;
-HEDLEY_PRAGMA(GCC diagnostic push)
-HEDLEY_PRAGMA(GCC diagnostic ignored "-Wignored-attributes")
-    using return_type = std::add_pointer_t<std::add_const_t<node_type>>;
-HEDLEY_PRAGMA(GCC diagnostic pop)
+    using return_type = const typename DpfKey::interior_node_t *;
     using iterator_type = return_type;
 
     nonmemoizing_path_memoizer()
@@ -96,7 +118,7 @@ HEDLEY_PRAGMA(GCC diagnostic pop)
     HEDLEY_NO_THROW
     HEDLEY_ALWAYS_INLINE
     HEDLEY_CONST
-    std::size_t assign_x(const dpf_type & dpf, input_type) noexcept
+    std::size_t assign_x(const dpf_type & dpf, input_type) noexcept override
     {
         if (std::addressof(dpf_->get()) != std::addressof(dpf)) v = dpf.root;
         return 1;
@@ -104,14 +126,17 @@ HEDLEY_PRAGMA(GCC diagnostic pop)
 
     HEDLEY_NO_THROW
     HEDLEY_ALWAYS_INLINE
-    node_type & operator[](std::size_t) noexcept { return v; }
+    node_type & operator[](std::size_t) noexcept override
+    {
+        return v;
+    }
 
-    return_type begin() const noexcept
+    return_type begin() const noexcept override
     {
         return std::addressof(v);
     }
 
-    return_type end() const noexcept
+    return_type end() const noexcept override
     {
         return std::addressof(v) + 1;
     }
